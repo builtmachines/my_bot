280acaa2d7b587e26eea242301154d9ba96cc314
maker <builtmachines@gmail.com>
2025-09-26 22:58:43 +0530

    Initial commit with ROS2 robot code

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -1,5 +1,7 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
@@ -10,51 +12,59 @@ from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
+
+
 def generate_launch_description():
 
-    package_name = 'my_bot'  # your package name
 
-    # Include robot_state_publisher launch
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='my_bot' #<--- CHANGE ME
+
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-            )
-        ]),
-        launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    # Twist mux
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )])
+    # )
+
+
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
-        package="twist_mux",
-        executable="twist_mux",
-        parameters=[twist_mux_params],
-        remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-    )
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
 
-    # Robot description via xacro (Humble-friendly)
-    robot_description = Command([
-        'xacro ',
-        os.path.join(get_package_share_directory(package_name), 'urdf', 'my_robot.urdf.xacro'),
-        ' sim_mode:=false'  # set to true if you want simulation
-    ])
+    
 
-    # Controller manager
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description}, controller_params_file]
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
     )
+
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
-    # Spawners
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
+
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
@@ -64,9 +74,10 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
+
     delayed_joint_broad_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
@@ -74,11 +85,31 @@ def generate_launch_description():
         )
     )
 
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+
+
+    # Launch them all!
     return LaunchDescription([
         rsp,
+        # joystick,
         twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
     ])
-
